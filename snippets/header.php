2d64dadda099ba30ac09714ba0b3d615128cf709
hexaF6-NinjaMC <103971610+hexaF6-NinjaMC@users.noreply.github.com>
2024-02-27 09:08:15 -0600

    Update header.php

--- snippets/header.php
+++ snippets/header.php
@@ -1,11 +1,11 @@
 <header>
-    <a href="/phpmotors/"><img src="/phpmotors/images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
+    <a href="/"><img src="/images/site/logo.png" alt="PHP Motors logo" id="logo"></a>
     <?php
         if (isset($_SESSION['clientData'])) {
             $clientData = $_SESSION['clientData'];
-            echo "<span id='welcome-msg'>Welcome, <a href='/phpmotors/accounts/?action=logged-in'>$clientData[clientFirstname]</a> | <a href='/phpmotors/accounts/?action=logout'>Logout</a></span>";
+            echo "<span id='welcome-msg'>Welcome, <a href='/accounts/?action=logged-in'>$clientData[clientFirstname]</a> | <a href='/accounts/?action=logout'>Logout</a></span>";
         } else {
-            echo '<a href="/phpmotors/accounts/" id="my-account">My Account</a>';
+            echo '<a href="/accounts/" id="my-account">My Account</a>';
         }
      ?>
     <nav>
